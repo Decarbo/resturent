f729b39b290e1d931f014ca0510e9857b01c471b
siyaar <shubham.21b1531196@abes.ac.in>
2024-12-31 23:21:09 +0530

    added footer components

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import './App.css';
 import Card from '../src/Components/Card';
 import Navbar from '../src/Components/Navbar';
 import SidePanel from './Components/sidePannel';
+import Footer from './Components/Footer';
 
 function App() {
-	return (<>
-		<div>
-			<SidePanel/>
-		</div>
-		<div className='flex flex-col gap-6  '>
+	return (
+	
+
+		{/*<div className='flex flex-col gap-6  '>
 			<div className='sticky top-0 z-50 '>
 				<Navbar />
 			</div>
 			<div className=" flex justify-between p-2 ">
 				<Card />
-			</div>
 			
-		</div>
-		</>
+			
+			
+			
+		</div>*/} 
+		
+		
+			
+		
 	);
 }
 
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./footer.css"; // Ensure the CSS file is correctly imported
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Logo */}
+        <div className="footer-logo">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5787/5787016.png"
+            alt="Restaurant Logo"
+          />
+        </div>
+
+        {/* Social Links */}
+        <div className="footer-social">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/733/733547.png"
+              alt="Facebook"
+              className="social-icon"
+            />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/733/733558.png"
+              alt="Instagram"
+              className="social-icon"
+            />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/733/733579.png"
+              alt="Twitter"
+              className="social-icon"
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
